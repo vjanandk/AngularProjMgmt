63640585e5ae3297da8d37cd84d51a4d5c1c743d
Vj <tanand.vijay@gmail.com>
2019-09-11 22:46:58 -0500

    user & task changes

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -27,30 +27,34 @@ export class RestService {
   }
 
   addUser(user): Observable<any> {
-    console.log("Add User : ", user);
+    console.log("Rest - addUser : ", user);
     return this.http.post<any>(endPoint + 'users', JSON.stringify(user), httpOptions).pipe(
-      tap((user) => console.log(`added user ', ${user}`)),
       catchError(this.handleError<any>('addUser'))
     );
   }
 
-  putUser(user): Observable<any> {
-    console.log("Put User : ", user);
-    return this.http.put<any>(endPoint + 'users', JSON.stringify(user), httpOptions).pipe(
-      tap((user) => console.log(`put user ', ${user}`)),
-      catchError(this.handleError<any>('putUser'))
-    );
-  }
-
   getUsers(): Observable<any> {
+    console.log(`Rest - getUsers`);
     return this.http.get(endPoint + 'users').pipe(
       map(this.extractData));
   }
 
+  getUser(userId): Observable<any> {
+    console.log(`Rest - getUser : ${userId}`);
+    return this.http.get<any>(endPoint + 'users' + '/' + userId).pipe(
+      map(this.extractData));
+  }
+
+  putUser(user): Observable<any> {
+    console.log("Rest - putUser : ", user);
+    return this.http.put<any>(endPoint + 'users', JSON.stringify(user), httpOptions).pipe(
+      catchError(this.handleError<any>('putUser'))
+    );
+  }
+
   addPrjoect(project): Observable<any> {
-    console.log("Add Project : ", project);
+    console.log("Rest - addProject : ", project);
     return this.http.post<any>(endPoint + 'projects', JSON.stringify(project), httpOptions).pipe(
-      tap((project) => console.log("Added Project ", project)),
       catchError(this.handleError<any>('addProject'))
     );
   }
@@ -65,6 +69,43 @@ export class RestService {
       map(this.extractData));
   }
 
+  addParentTask(ptask): Observable<any> {
+    console.log("Rest - addParentTask : ", ptask);
+    return this.http.post<any>(endPoint + 'parenttasks', JSON.stringify(ptask), httpOptions).pipe(
+      catchError(this.handleError<any>('addParentTask'))
+    );
+  }
+
+  getParentTasks(): Observable<any> {
+    return this.http.get(endPoint + 'parenttasks').pipe(
+      map(this.extractData));
+  }
+
+  getParentTask(ptaskName): Observable<any> {
+    return this.http.get(endPoint + `projects/${ptaskName}`).pipe(
+      map(this.extractData));
+  }
+
+  addTask(task): Observable<any> {
+    console.log("Rest - addTask : ", task);
+    return this.http.post<any>(endPoint + 'tasks', JSON.stringify(task), httpOptions).pipe(
+      catchError(this.handleError<any>('addTask'))
+    );
+  }
+
+  getTasks(): Observable<any> {
+    return this.http.get(endPoint + 'tasks').pipe(
+      map(this.extractData));
+  }
+
+  getTask(ptaskName): Observable<any> {
+    return this.http.get(endPoint + `tasks/${ptaskName}`).pipe(
+      map(this.extractData));
+  }
+
+
+
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
@@ -75,3 +116,21 @@ export class RestService {
     }
   }
 }
+
+
+
+// updUserProj(userId, projId): Observable<any> {
+//   console.log(`Rest - updUserProj : ${userId}, ${projId}`);
+//   return this.http.put<any>(endPoint + 'users/updproj' + '/' + userId + '/' + projId, httpOptions).pipe(
+//     tap((user) => console.log(`updUserProj : ${userId}, ${projId}`)),
+//     catchError(this.handleError<any>('updUserProj'))
+//   );
+// }
+
+// updUserTask(userId, taskId): Observable<any> {
+//   console.log(`updUserTask : ${userId}, ${taskId}`);
+//   return this.http.put<any>(endPoint + 'users' + '/' + userId + '/' + taskId, httpOptions).pipe(
+//     tap((user) => console.log(`updUserTask : ${userId}, ${taskId}`)),
+//     catchError(this.handleError<any>('updUserTask'))
+//   );
+// }
